d8e152de257dcba002c213926ddbd880db572247
githubuiux <uiuxista@gmail.com>
2023-11-09 11:41:58 +0300

    Initial Commit

---
+++ ScrollableTabView/ScrollableTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrollableTabViewApp.swift
+//  ScrollableTabView
+//
+//  Created by Levent on 9.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct ScrollableTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
